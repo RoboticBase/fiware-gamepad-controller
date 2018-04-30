14ac13479efb775b83687ae78b810e0fa279db22
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-04-30 10:29:23 +0900

    publish event to mqtt using paho

--- src/controller.py
+++ src/controller.py
@@ -7,9 +7,13 @@ from logging import getLogger
 import pygame
 from pygame.locals import JOYBUTTONDOWN, JOYBUTTONUP, JOYHATMOTION, JOYAXISMOTION
 
+import paho.mqtt.client as mqtt
+
 from src.utils import findItem
 
 logger = getLogger(__name__)
+
+TOPIC_KEY = 'controller'
 
 class ControllerError(Exception):
     def __init__(self, *args, **kwargs):
@@ -25,10 +29,28 @@ class Controller:
             pygame.joystick.init()
             controller = pygame.joystick.Joystick(0)
             controller.init()
-            self.conf = conf
+            self.__conf = conf
+            self.__client = None
             logger.info('initialized %s', conf.name)
         except pygame.error as e:
             raise ControllerError('init error', cause=e)
+
+    @property
+    def client(self):
+        if self.__client is None:
+            def __on_connect(client, userdata, flags, response_code):
+                logger.info('connected mqtt broker[%s:%d], response_code=%d',
+                            self.__conf.mqtt.host, self.__conf.mqtt.port, response_code)
+            def __on_disconnect(client, userdata, response_code):
+                logger.info('disconnected mqtt broker, response_code=%d', response_code)
+
+            self.__client = mqtt.Client(protocol=mqtt.MQTTv311)
+            self.__client.on_connect = __on_connect
+            self.__client.on_disconnect = __on_disconnect
+
+            self.__client.connect(self.__conf.mqtt.host, port=self.__conf.mqtt.port, keepalive=60)
+            self.__client.loop_start()
+        return self.__client
 
     def describeEvents(self):
         logger.info('start describing...')
@@ -47,17 +69,25 @@ class Controller:
         logger.info('start publishing...')
         def callback(event):
             if event.type == JOYBUTTONDOWN:
-                item = findItem(self.conf.controller.buttons, lambda item: item.key == event.button)
+                item = findItem(self.__conf.controller.buttons, lambda item: item.key == event.button)
                 if item:
-                    logger.info('published %s', item.value)
+                    self.__publish_mqtt(item.value)
             elif event.type == JOYHATMOTION:
-                item = findItem(self.conf.controller.hats,
+                item = findItem(self.__conf.controller.hats,
                                 lambda item: item.x == event.value[0] and item.y == event.value[1])
                 if item:
-                    logger.info('published %s', item.value)
+                    self.__publish_mqtt(item.value)
             else:
                 logger.debug('ignore event, %s', event)
         self.__subscribeEvent(callback)
+
+    def __publish_mqtt(self, payload):
+        topic = findItem(self.__conf.mqtt.topics, lambda item: item.key == TOPIC_KEY)
+        if topic:
+            self.client.publish(topic.value, payload)
+            logger.info('published "%s" to "%s"', payload, topic.value)
+        else:
+            logger.warning('no topic found, key=%s', TOPIC_KEY)
 
     def __subscribeEvent(self, callback):
         signal.signal(signal.SIGINT, self.__stopLoop)
@@ -70,5 +100,9 @@ class Controller:
             raise ControllerError('subscribe event error', cause=e)
 
     def __stopLoop(self, signal, frame):
+        if self.__client is not None:
+            self.__client.loop_stop()
+            self.__client.disconnect()
+
         logger.info('stop main loop')
         exit(0)
